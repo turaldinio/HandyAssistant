2469958159ff75816c8eab57a0f881d5ce8c199f
Tural Guluev <tural-98@bk.ru>
2022-06-24 19:23:12 +0300

    second task

--- src/Client.java
+++ src/Client.java
@@ -5,18 +5,35 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Client {
+
     public static void main(String[] args) {
-        try (Socket clientSocket = new Socket(InetAddress.getLocalHost(), 8089)) {
+        try (Socket clientSocket = new Socket("netology.homework", 8089)) {
+            Scanner scan = new Scanner(System.in);
             PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream(), true);
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            printWriter.println("turaldinio");
+            System.out.println(reader.readLine());
 
-            String response = reader.readLine();
-            System.out.println(response);
+            printWriter.println(scan.nextLine());
+
+            System.out.println(reader.readLine());
+
+            printWriter.println(scan.nextLine());
+
+            System.out.println(reader.readLine());
+
+            printWriter.println(scan.nextLine());
+
+            System.out.println(reader.readLine());
+
+            System.out.println(reader.readLine());
+            System.out.println(reader.readLine());
+            System.out.println(reader.readLine());
+
 
         } catch (UnknownHostException e) {
             e.printStackTrace();
--- src/Server.java
+++ src/Server.java
@@ -4,23 +4,38 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Server {
+
     public static void main(String[] args) throws IOException {
         System.out.println("server is running");
+        Server server = new Server();
+
         try (ServerSocket serverSocket = new ServerSocket(8089)) {
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream(), true);
                 BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-                System.out.println("New connection accepted");
+                printWriter.println("welcome to the registration site netology.homework. Enter your name");
 
-                final String name = reader.readLine();
+                String name = reader.readLine();
 
-                printWriter.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                printWriter.println(String.format("Hi %s, enter your age", name));
+                String age = reader.readLine();
+
+                printWriter.println("Ok.. and finally enter your sex");
+                String sex = reader.readLine();
+
+                printWriter.println(String.format("You have successfully registered your data:" +
+                        "\nName- %s\nAge- %s\nSex- %s", name, age, sex));
+
+
             }
 
         }
     }
+
 }
